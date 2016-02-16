512b2400bb07683bc288be682a63e066ef59c6df
Trevor Peitzman <trevor.peitzman@gmail.com>
2016-02-15 19:50:56 -0800

    Initial Upload......
    WAIT.........
    I'm Just gettin started!

---
+++ src/proteinSynth/ProteinSynth.java
@@ -0,0 +1,14 @@
+package proteinSynth;
+
+//import java.util.ArrayList;
+
+public class ProteinSynth {
+	
+	public static void main(String[] args) {
+		int bill = 10;
+		char b = 0;
+		int v = b + bill;
+		System.out.println("billl " + v);
+	}
+}
+	

7cd19cd40307642cb38b4b2fcbd90c9c68a462a1
Trevor Peitzman <trevor.peitzman@gmail.com>
2016-02-15 21:44:40 -0800

    Added Font, more text, and correct input
    + Ubuntu Font
    + Kindly Text
    + "Sythesize!" and "Reset" bttns
    + Proper Text Boxes
    + Cool Lookin Header

--- src/proteinSynth/ProteinSynth.java
+++ src/proteinSynth/ProteinSynth.java
@@ -1,14 +1,27 @@
 package proteinSynth;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.Scanner;
+
+import com.sun.org.apache.xpath.internal.functions.Function;
+
 //import java.util.ArrayList;
 
 public class ProteinSynth {
 	
 	public static void main(String[] args) {
-		int bill = 10;
+		//BufferedReader in = new BufferedReader(new Reader(System.in));
+		InputStream Scanner = ;
+				//System.in;
+		int RNA = 10;
 		char b = 0;
-		int v = b + bill;
-		System.out.println("billl " + v);
+		int v = b + RNA;
+		System.out.println("Your RNA Sequence Was: " + RNA);
+	
+		
+		
 	}
 }
 	
